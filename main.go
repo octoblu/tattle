52ed8af31336c9865842e086675be17178b1031a
sqrt <sqrt@octoblu.com>
2016-01-26 14:11:55 -0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "governator"
+	app.Action = run
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:   "application, a",
+			EnvVar: "GOVERNATOR_APPLICATION",
+			Usage:  "Name of the application that failed us",
+		},
+		cli.StringFlag{
+			Name:   "service, s",
+			EnvVar: "GOVERNATOR_SERVICE",
+			Usage:  "Name of the service that is specifically at fault",
+		},
+		cli.StringFlag{
+			Name:   "uri, u",
+			EnvVar: "GOVERNATOR_URI",
+			Usage:  "Uri to POST to with a cancellation notice",
+		},
+	}
+	app.Run(os.Args)
+}
+
+func run(context *cli.Context) {
+	applicationName := context.String("application")
+	serviceName := context.String("service")
+	uri := context.String("uri")
+
+	res, err := http.PostForm(uri, url.Values{"applicationName": {applicationName}, "serviceName": {serviceName}})
+	if err != nil {
+		log.Panicf("Failure to post to uri: %v", err.Error())
+	}
+	if res.StatusCode != 201 {
+		log.Panicf("Cancellation failed with code '%v'", res.StatusCode)
+	}
+}
